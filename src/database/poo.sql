28bfb5574ea74c884b6f4d977326a2011ff1b0e1
marcela-celani <marcelacelani.ti@gmail.com>
2023-10-30 20:38:46 -0300

    refatoraçao arquitetura

--- src/database/poo.sql
+++ src/database/poo.sql
@@ -1,22 +1,24 @@
--- Active: 1675168195024@@127.0.0.1@3306
+-- Active: 1698703847063@@127.0.0.1@1433
 CREATE TABLE users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    created_at TEXT NOT NULL
+    created_at TEXT NOT NULL DEFAULT (DATETIME())
 );
 
 CREATE TABLE accounts (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     owner_id TEXT NOT NULL,
     balance REAL DEFAULT (0) NOT NULL,
-    created_at TEXT NOT NULL,
+    created_at TEXT NOT NULL DEFAULT (DATETIME()),
     FOREIGN KEY (owner_id) REFERENCES users (id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
 
+SELECT * FROM users;
+DROP TABLE accounts;
 INSERT INTO users (id, name, email, password)
 VALUES
 	('u001', 'Fulano', 'fulano@email.com', 'fulano123'),
